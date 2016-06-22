7011552dfe8d38217090f90cbcb8f61d9609e891
Alexander Tiedmann <alexander.tiedmann@googlemail.com>
2016-06-22 14:29:36 +0200

    Abschluss Aufgabe 10

--- Arrays/src/Arrays.java
+++ Arrays/src/Arrays.java
@@ -1,6 +1,68 @@
 public class Arrays {
 
-    public static void main(String[] args) {
-	    
+    public double[] kehreUm(double[] zahlenArray){
+        double[] rueckArray = new double[zahlenArray.length];
+        for (int i=0; i < zahlenArray.length; i++){
+            rueckArray[i] = zahlenArray[zahlenArray.length - i-1];
+        }
+        return rueckArray;
+    }
+
+    public String arrayToString(double[] zahlen){
+        String text = new String();
+        for (int i=0; i < zahlen.length; i++){
+            text = text + zahlen[i] + ",";
+        }
+        return text;
+    }
+
+    public double mittelwert (double[] zahlen){
+        double mitt=0.0;
+        double tmp=0.0;
+        for (int i=0; i < zahlen.length;i++){
+            mitt = mitt + zahlen[i];
+        }
+        mitt = mitt / zahlen.length;
+        return mitt;
+    }
+
+    public int diff (int[] zahlen){
+        int groesste = groesste(zahlen);
+        int kleinste = kleinste(zahlen);
+        return groesste - kleinste;
+    }
+
+    public void zdAA(int[] [] zw){
+        for (int i=0; i < zw.length;i++){
+            System.out.println(intArrayToString(zw[i]));
+        }
+    }
+
+    public String intArrayToString(int[] zahlen){
+        String text = new String();
+        for (int i=0; i < zahlen.length; i++){
+            text = text + zahlen[i] + ",";
+        }
+        return text;
+    }
+
+    private int groesste(int[] zahlen) {
+        int zahl = zahlen[0];
+        for (int i = 0; i < zahlen.length; i++) {
+            if (zahl < zahlen[i]){
+                zahl=zahlen[i];
+            }
+        }
+        return zahl;
+    }
+
+    private int kleinste(int[] zahlen) {
+        int zahl = zahlen[0];
+        for (int i = 0; i < zahlen.length; i++) {
+            if (zahl > zahlen[i]){
+                zahl=zahlen[i];
+            }
+        }
+        return zahl;
     }
 }
---
+++ Arrays/src/TestArray.java
@@ -0,0 +1,25 @@
+public class TestArray {
+    public static void main(String[] args) {
+        System.out.println("##################double Array##################");
+        double[] doubleArray = {1.5,2.7,3.5};
+        Arrays ar = new Arrays();
+        System.out.println("Umgekehrtes Array: " + ar.arrayToString(ar.kehreUm(doubleArray)));
+        System.out.println("Mittelwert des Arrays: " + ar.mittelwert(doubleArray));
+
+        System.out.println("##################int Array##################");
+        int[] intArray = {1,2,3,4,5,12,21};
+        System.out.println("Differenz groesste und niedriegste: " + ar.diff(intArray));
+
+        System.out.println("##################zweidimensionales Array##################");
+        int [][] arrayU = new int [3][];
+        arrayU [0] = new int [2];
+        arrayU [1] = new int [3];
+        arrayU [2] = new int [1];
+
+        arrayU [0][1] = 2;
+        arrayU [1][0] = 3;
+        arrayU [1][2] = 5;
+        arrayU [2][0] = 4;
+        ar.zdAA(arrayU);
+    }
+}
