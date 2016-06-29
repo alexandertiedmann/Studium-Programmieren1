beb8b71986bfa46ab02ef95fc372f964501b34e3
Alexander Tiedmann <alexander.tiedmann@googlemail.com>
2016-06-29 17:05:47 +0200

    Hinzufügen Aufgabe 11

--- Arrays/src/Arrays.java
+++ Arrays/src/Arrays.java
@@ -10,21 +10,23 @@ public class Arrays {
         return rueckArray;
     }
 
-    public String ArrayToString(double[] zahlen){
+    public String arrayToString(double[] zahlen){
         //statt einen String zurückzugeben hätte an dieser Stelle auch direkt ausgegeben werden können
         String text = new String();
         for (int i=0; i < zahlen.length; i++){
             text = text + zahlen[i] + ",";
         }
+        text = text.substring(0, text.length() -1 );
         return text;
     }
 
-    public String ArrayToString(int[] zahlen){
+    public String arrayToString(int[] zahlen){
         //statt einen String zurückzugeben hätte an dieser Stelle auch direkt ausgegeben werden können
         String text = new String();
         for (int i=0; i < zahlen.length; i++){
             text = text + zahlen[i] + ",";
         }
+        text = text.substring(0, text.length() -1 );
         return text;
     }
 
@@ -51,7 +53,7 @@ public class Arrays {
         //das Arrays in der 2. dimension ausgeben
         //da alle Zahlen gleich viele Stellen haben, hier kein printf
         for (int i=0; i < zw.length;i++){
-            System.out.println(ArrayToString(zw[i]));
+            System.out.println(arrayToString(zw[i]));
         }
     }
 
@@ -77,5 +79,100 @@ public class Arrays {
             }
         }
         return zahl;
+    }
+
+    /**
+     * Die Methode erzeugt ein neues Array mit der Summe der Laengen der ersten beiden
+     * und uebernimmt die Werte aus den ersten beiden Arrays.
+     * @param zahlen1 wird als erstes in das neue Array uebernommen
+     * @param zahlen2 wird als zweites in das neue Array uebernommen
+     * @return die zu einem Array verketteten Arrays
+     */
+    public int[] verkette(int[] zahlen1, int[] zahlen2){
+        int[] kettArray = new int[zahlen1.length + zahlen2.length];
+        //einsortieren des ersten Arrays in das verkettete Array
+        for (int i = 0; i<zahlen1.length; i++ ){
+            kettArray[i] = zahlen1[i];
+        }
+        //einsortieren des zweiten Arrays in das verkettete Array
+        int zahl = 0;
+        for (int j = zahlen1.length; j < kettArray.length; j++){
+            kettArray[j] = zahlen2[zahl];
+            zahl++;
+        }
+        return kettArray;
+    }
+
+    /**
+     * Der uebergebene Wert wird gesucht und vom letzten  gefundenen
+     * Treffer wird die Position im Array angegeben.
+     * Ansonsten wird -1 zurueckgegeben
+     * @param zahlen zu uebergebenes Array in dem gesucht wird
+     * @param suche Zahl nach der gesucht werden soll
+     * @return -1 oder Position des Treffers
+     */
+    public int suche (int[] zahlen, int suche){
+        boolean gefunden = false;
+        int index=-1;
+        for (int i = 0; i < zahlen.length; i++){
+            if ( suche == zahlen[i] ) {
+                gefunden = true;
+                index = i;
+            }
+        }
+        if ( gefunden ) {
+            return index;
+        }else{
+            return -1;
+        }
+    }
+
+    /**
+     * Ein Array wir linear durchsucht und die Position im Array zurueck gegeben
+     * Ansonsten -1
+     * @param zahlen zu durchsuchendes Array
+     * @param suche zu suchende Zahl
+     * @return Position der Zahl im Array
+     */
+    public int linSuche(int[] zahlen, int suche){
+        int gefunden = -1;
+        int i=0;
+        while ( gefunden < 0){
+            if ( i >= zahlen.length){
+                gefunden = -1;
+                break;
+            }
+            if ( suche == zahlen[i]){
+                gefunden = i;
+            }
+            i++;
+        }
+        return gefunden;
+    }
+
+    /**
+     * Ein uebergebenes Array wird mit Hilfe der binaeren Suche
+     * und iterativ nach eier bestimmten Zahl durchsucht
+     * @param zahlen zu durchsuchendes Array
+     * @param suche zu suchende Zahl
+     * @return Position der gefundenen Zahl
+     */
+    public int binSucheIter(int[] zahlen, int suche){
+        int gefunden = -1;
+
+        return gefunden;
+    }
+
+    /**
+     * Ein uebergebenes Array wird mit Hilfe der binaeren Suche
+     * nach eier bestimmten Zahl durchsucht
+     * @param zahlen zu durchsuchendes Array
+     * @param suche zu suchende Zahl
+     * @return Position der gefundenen Zahl
+     */
+    public int binSuche(int[] zahlen, int suche){
+        int gefunden = -1;
+        
+        return gefunden;
     }
 }
--- Arrays/src/TestArray.java
+++ Arrays/src/TestArray.java
@@ -1,9 +1,62 @@
 public class TestArray {
     public static void main(String[] args) {
+        Arrays ar = new Arrays();
+        System.out.println("Aufgabe 11.5:");
+        aufgabe115(ar);
+        System.out.println("Aufgabe 11.6:");
+        aufgabe116(ar);
+    }
+    public static void aufgabe115(Arrays ar){
+        int[] intArrayx = {1,2,3,4,5,6,7,8,9,10};
+        int test1 = ar.linSuche(intArrayx,5);
+        int test2 = ar.linSuche(intArrayx,11);
+        if ( test1 == -1 ){
+            System.out.println("Element nicht gefunden");
+        }else{
+            System.out.println(test1);
+        }
+        if ( test2 == -1 ){
+            System.out.println("Element nicht gefunden");
+        }else{
+            System.out.println(test2);
+        }
+    }
+    public static void aufgabe116(Arrays ar){
+        int [] intArray = {2,4,6,8,10,12,14,16};
+        aufgabe116Suche(intArray,ar,1);
+        aufgabe116Suche(intArray,ar,2);
+        aufgabe116Suche(intArray,ar,6);
+        aufgabe116Suche(intArray,ar,11);
+        aufgabe116Suche(intArray,ar,16);
+        aufgabe116Suche(intArray,ar,17);
+
+        aufgabe116Iter(intArray,ar,1);
+        aufgabe116Iter(intArray,ar,2);
+        aufgabe116Iter(intArray,ar,6);
+        aufgabe116Iter(intArray,ar,11);
+        aufgabe116Iter(intArray,ar,16);
+        aufgabe116Iter(intArray,ar,17);
+    }
+    private static void aufgabe116Iter(int[] intArray, Arrays ar, int suche){
+        int temp = ar.binSucheIter(intArray,suche);
+        if ( temp > -1 ){
+            System.out.println("Suche " + suche + ": " + temp);
+        }else{
+            System.out.println("Element (" + suche + ") nicht gefunden!");
+        }
+    }
+    private static void aufgabe116Suche(int[] intArray, Arrays ar, int suche){
+        int temp = ar.binSuche(intArray,suche);
+        if ( temp > -1 ){
+            System.out.println("Suche " + suche + ": " + temp);
+        }else{
+            System.out.println("Element (" + suche + ") nicht gefunden!");
+        }
+    }
+    public static void aufgabe10(Arrays ar){
         System.out.println("##################double Array##################");
         double[] doubleArray = {1.5,2.7,3.5};
-        Arrays ar = new Arrays();
-        System.out.println("Umgekehrtes Array: " + ar.ArrayToString(ar.kehreUm(doubleArray)));
+        System.out.println("Umgekehrtes Array: " + ar.arrayToString(ar.kehreUm(doubleArray)));
         System.out.println("Mittelwert des Arrays: " + ar.mittelwert(doubleArray));
 
         System.out.println("##################int Array##################");
@@ -21,4 +74,26 @@ public class TestArray {
         arrayU [2][0] = 4;
         ar.zdAA(arrayU);
     }
+    public static void aufgabe111(Arrays ar){
+        int[] intArray1 = {1,2,3,4,5,12,21};
+        int[] intArray2 = {1,2,3,4,5,6,7,8,9,10};
+        System.out.println("1. Array: " + ar.arrayToString(intArray1));
+        System.out.println("2. Array: " + ar.arrayToString(intArray2));
+        System.out.println("Verkettetes Array: " + ar.arrayToString(ar.verkette(intArray1,intArray2)));
+    }
+    public static void aufgabe112(Arrays ar){
+        int[] intArrayx = {1,2,3,4,5,6,7,8,9,10};
+        aufgabe112(intArrayx,4,ar);
+        aufgabe112(intArrayx,11,ar);
+    }
+    private static void aufgabe112(int[] intArrayx, int zahl,Arrays ar){
+        int temp = ar.suche(intArrayx,zahl);
+        if ( temp > -1 ){
+            System.out.println("Suche " + zahl + ": " + temp);
+        }else{
+            System.out.println("Element (" + zahl + ") nicht gefunden!");
+        }
+    }
+
+
 }
